24bc83b1345934a5b0e207d8db87d9ab67da6d08
Taylor Fritz <tayfritz01@gmail.com>
2020-07-16 13:55:14 -0400

    Timer added to page

---
+++ script.js
@@ -0,0 +1,31 @@
+var countdownDate = new Date("July 31, 2020 11:59:59").getTime(); 
+
+var myfunc = setInterval(function() {
+
+    let now = new Date().getTime();
+
+    let timeLeft = countdownDate - now;
+
+
+    let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+
+    document.getElementById("days").innerHTML = "Days: " + days
+    document.getElementById("hours").innerHTML = "Hours: " + hours 
+    document.getElementById("mins").innerHTML = "Minutes: " + minutes 
+    document.getElementById("secs").innerHTML = "Seconds: " + seconds
+
+
+    if (timeLeft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("days").innerHTML = ""
+        document.getElementById("hours").innerHTML = "" 
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+        document.getElementById("message").innerHTML = "TIMES UP!!";
+    }
+
+}, 1000)
