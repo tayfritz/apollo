1552a87bd5fd0f7b9ab9e62c2747801601c34912
Taylor Fritz <tayfritz01@gmail.com>
2020-07-21 13:38:25 -0400

    Playing with conditions

--- javascript/countdown.js
+++ javascript/countdown.js
@@ -7,12 +7,16 @@ let endTimerMessage = document.createElement("p");
 let timerLocation = document.getElementsByTagName("main");
 
 // Conditional logic to prompt user to provide a date or to 'cancel' which will hide timer div
-if (countdownDate == null || countdownDate == " ") {
+if (countdownDate === null  || countdownDate == "") {
     timer.style.display = "none";
 }
-                // } else if (countdownDate.value == undefined) {
+                // } else if (countdownDate.value === undefined) {
                 //     alert("You must provide a date or press 'cancel");
 
+                // }
+
+                // if (countdownDate === "") {
+                //     alert("You must provide a date or press 'cancel'");
                 // }
 
 
