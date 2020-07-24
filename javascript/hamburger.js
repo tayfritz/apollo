ff8e07a87530dd1965ee0b8d55c6f6d29ad026f5
Taylor Fritz <tayfritz01@gmail.com>
2020-07-23 20:02:57 -0400

    Work on hamburger.js

---
+++ javascript/hamburger.js
@@ -0,0 +1,6 @@
+let ham1 = document.getElementById("ham1");
+let ham2 = document.getElementById("ham2");
+let ham3 = document.getElementById("ham3");
+let hamburgerMenu = document.getElementById("hamburger");
+
+
