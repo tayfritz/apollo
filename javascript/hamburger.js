51511a34b6381b25fb3e7c4642ddd1d72c1e9dfe
Taylor Fritz <tayfritz01@gmail.com>
2020-07-24 20:01:21 -0400

    Work with hamburger and css styling of nav with media queries

--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -1,7 +1,14 @@
+// Variables
 let ham1 = document.getElementById("ham1");
 let ham2 = document.getElementById("ham2");
 let ham3 = document.getElementById("ham3");
 let hamburgerMenu = document.getElementById("hamburger");
 
+// Event Listeners
+// hamburgerMenu.addEventListener("click", () => {
+//     ham1.style.display = "none";
+//     ham2.style.transform = "rotate(60deg)";
+//     ham3.style.transform = "rotate(-60deg)";
+// });
 
 //  transform: rotate(180deg);
