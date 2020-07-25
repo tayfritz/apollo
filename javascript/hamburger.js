8b45d7cecccf7ed2b7668dbe6c1d00bdda274471
Taylor Fritz <tayfritz01@gmail.com>
2020-07-25 12:41:32 -0400

    Hamburger menu interactivity

--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -1,8 +1,20 @@
 // Variables
-let ham1 = document.getElementById("ham1");
-let ham2 = document.getElementById("ham2");
-let ham3 = document.getElementById("ham3");
+// let ham1 = document.getElementById("ham1");
+// let ham2 = document.getElementById("ham2");
+// let ham3 = document.getElementById("ham3");
 let hamburgerMenu = document.getElementById("hamburger");
+let expand = document.getElementById("expanded-nav");
+
+function onClickMenu() {
+    document.getElementById("hamburger").classList.toggle("change");
+    document.getElementById("expanded-nav").classList.toggle("exp");
+    if (expand.style.display === "block") {
+        expand.style.display = "none";
+    } else {
+        expand.style.display = "block";
+    }
+}
+
 
 // Event Listeners
 // hamburgerMenu.addEventListener("click", () => {
