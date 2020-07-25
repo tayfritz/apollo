faf9e016430981aa6b659907867b837f4cf389ce
Taylor Fritz <tayfritz01@gmail.com>
2020-07-25 15:42:59 -0400

    Debugging hamburger with media queries

--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -4,23 +4,16 @@
 // let ham3 = document.getElementById("ham3");
 let hamburgerMenu = document.getElementById("hamburger");
 let expand = document.getElementById("expanded-nav");
-
-function onClickMenu() {
-    document.getElementById("hamburger").classList.toggle("change");
-    document.getElementById("expanded-nav").classList.toggle("exp");
-    if (expand.style.display === "block") {
-        expand.style.display = "none";
-    } else {
-        expand.style.display = "block";
-    }
-}
+let intFrameWidth = window.innerWidth;
 
 
-// Event Listeners
-// hamburgerMenu.addEventListener("click", () => {
-//     ham1.style.display = "none";
-//     ham2.style.transform = "rotate(60deg)";
-//     ham3.style.transform = "rotate(-60deg)";
-// });
+function onClickMenu() { 
+        document.getElementById("hamburger").classList.toggle("change");
+        document.getElementById("expanded-nav").classList.toggle("exp");
+        if (expand.style.display === "block") {
+            expand.style.display = "none";
+        } else {
+            expand.style.display = "block";
+        }
+}  
 
-//  transform: rotate(180deg);
