809778018977e86649bc5c745293d812ac73c137
Taylor Fritz <tayfritz01@gmail.com>
2020-07-26 22:41:53 -0400

    Responsiveness added to .welcome and .form

--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -1,12 +1,6 @@
-// Variables
-// let ham1 = document.getElementById("ham1");
-// let ham2 = document.getElementById("ham2");
-// let ham3 = document.getElementById("ham3");
+
 let hamburgerMenu = document.getElementById("hamburger");
 let expand = document.getElementById("expanded-nav");
-var useHam = window.matchMedia("(min-width: 768px)");
-let body = document.querySelector("body").width;
-
 
 
 
