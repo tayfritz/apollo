98fa53bded4c4d6c708379f82b836232d686d426
Taylor Fritz <tayfritz01@gmail.com>
2020-07-28 18:05:27 -0400

    Continuing to work through form validation

--- javascript/form.js
+++ javascript/form.js
@@ -1,14 +1,9 @@
-let signup = document.querySelector("press");
-let fname = document.querySelector("fname");
-let lname = document.querySelector("lname");
-let email = document.querySelector("email");
-let role = document.querySelector("role");
+// let signup = document.getElementById("press");
+var fname = document.getElementById("first-name");
+let email = document.getElementById("user-email");
+let role = document.getElementById("user-role"); 
+let lname = document.getElementById("last-name");
+let myForm = document.getElementById("myForm");
 
 
-function validate() {
-    if (document.forms.fname.value == "") {
-        alert('Please provide your name');
-        document.forms.fname.focus();
-        return false;
-    }
-}
+
