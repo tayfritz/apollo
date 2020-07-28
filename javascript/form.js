163108c6c6df23d58b77ed8a1d6e0ccf4e7b0714
Taylor Fritz <tayfritz01@gmail.com>
2020-07-28 18:27:11 -0400

    onClick function added to form to check for validity

--- javascript/form.js
+++ javascript/form.js
@@ -1,9 +1,31 @@
 // let signup = document.getElementById("press");
-var fname = document.getElementById("first-name");
-let email = document.getElementById("user-email");
-let role = document.getElementById("user-role"); 
-let lname = document.getElementById("last-name");
+let fname = document.getElementById("first_name");
+let lname = document.getElementById("last_name");
+let email = document.getElementById("user_email");
+let role = document.getElementById("user_role"); 
 let myForm = document.getElementById("myForm");
 
+function onClick() {
+    let fname = document.getElementById("first_name");
+    let lname = document.getElementById("last_name");
+    let email = document.getElementById("user_email");
+    let role = document.getElementById("user_role"); 
+    
+    if (fname.value == "") {
+        alert("You must provide a first name");
+        fname.focus()
+    }
 
+    if (lname.value == "") {
+        alert("You must provide a last name");
+    }
+
+    if (email.value == "") {
+        alert("You must provide an email.");
+    } 
+
+    if (role.value == "") {
+        alert("Please select your role from the drop down menu so we can best serve your needs.");
+    }
+}
 
